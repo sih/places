dad585e64b339f3e76b76a8388d9ded39db1c275
Sid Haniff <syed_haniff@hotmail.com>
2019-02-21 22:11:08 +0000

    Add swagger support

--- src/main/java/tech/dsoc/sockets/places/PlacesApp.java
+++ src/main/java/tech/dsoc/sockets/places/PlacesApp.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @RestController
 @EnableJpaRepositories
+@EnableSwagger2
 public class PlacesApp {
     public static void main(String[] args) {
         SpringApplication.run(PlacesApp.class, args);
